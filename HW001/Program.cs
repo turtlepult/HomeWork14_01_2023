521201552fde6719a2b06375ff4e5d9782277e98
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-15 13:57:34 +0500

    create a maby work programm

--- HW001/Program.cs
+++ HW001/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine("Введите первое число: ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int n2 = int.Parse(Console.ReadLine());
+
+if ((n2*n2) == n1)
+{
+     Console.WriteLine("Число: " + n1 + " является квадратом числа: " + n2);
+}
+else
+{
+    Console.WriteLine("Число: " + n1 + " НЕ является квадратом числа: " + n2);
+}
