c84509678e200cff70c85fd254a074e7dde84053
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-17 22:55:48 +0500

    created possibly a working program

--- HW008/Program.cs
+++ HW008/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Удалить вторую цифру трёхзначного числа
+int rn = new Random().Next(100, 1000);
+Console.WriteLine("Рандомное число: " + rn);
+
+string srn = rn.ToString();
+char i = srn[srn.Length - 2];
+Console.WriteLine("а это оно же, но без средней цифры: " + srn[srn.Length - 3] + srn[srn.Length - 1]);
