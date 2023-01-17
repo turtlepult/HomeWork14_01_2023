180e1cc4fc2f9966e5d940505f0d88093af5dc4c
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-17 23:10:29 +0500

    created possibly a working program

--- HW010/Program.cs
+++ HW010/Program.cs
@@ -1,1 +1,16 @@
 ﻿//Найти третью цифру числа или сообщить, что её нет
+int rn = new Random().Next(98, 102);
+Console.WriteLine("Рандомное число: " + rn);
+
+string srn = rn.ToString();
+int length = srn.Length;
+
+if (length<3)
+{
+    Console.WriteLine("В числе " + rn + " нет третей цифры");
+}
+else
+{
+    char i = srn[srn.Length - 1];
+    Console.WriteLine("И его третья цифра: " + i);
+}
